65eb87f737c435d6812bfa176bd7ba2defbf5d18
Bayanda <149507292+hubYanda@users.noreply.github.com>
2023-11-03 00:05:19 -0700

    Add files via upload
    echo "# hubYanda" >> README.md
    git init
    git add README.md
    git commit -m "first commit"
    git branch -M main
    git remote add origin https://github.com/hubYanda/hubYanda.git
    git push -u origin main

---
+++ home.php
@@ -0,0 +1,30 @@
+<?php
+
+session_start();
+
+
+if(isset($_SESSION['id']) && isset($_SESSION['username']));
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Home Page</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<h1> Hello, <?php echo $_SESSION['Username']; ?> </h1>
+	<a href="logout.php">Logout</a>
+</body>
+</html>
+
+<?php
+
+
+else{
+	header("Location: index.php");
+	exit();
+}
+?>
